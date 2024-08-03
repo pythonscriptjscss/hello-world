483cfee4dccbb738fb32486798a3009c561f349f
pythonscriptjscss <gameunreal44@gmail.com>
2024-08-03 20:30:38 +0530

    fix all this

---
+++ python-opencv/learn1.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+# img = cv.imread("D:\mystudy\pythonOCR\python-opencv\images\h.jpg")
+# cv.imshow("Cat",img)
+# cv.waitKey(0)
+
+capture = cv.VideoCapture("D:\mystudy\pythonOCR\python-opencv\Videos\eh.mp4")
+while True:
+    isTrue , frame = capture.read()
+    cv.imshow("Video",frame)
+    if cv.waitKey(20) & 0xFF == "d":
+        break
+
+capture.release()
+cv.destroyAllWindows() 
